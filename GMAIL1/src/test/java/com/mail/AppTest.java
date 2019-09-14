db7767b9204ca52c5b9ea44838b721cc4b2dee96
Ranjan <usert5558@gmail.com>
2019-09-14 15:55:14 +0000

    modify pom xml

--- GMAIL1/src/test/java/com/mail/AppTest.java
+++ GMAIL1/src/test/java/com/mail/AppTest.java
@@ -4,14 +4,8 @@ package com.mail;
 import org.testng.annotations.Test;
 //import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
